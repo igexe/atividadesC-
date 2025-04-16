801f58d16416d32d50dede6599ae593e11cd771c
wollii <wollii@wollii.wollii>
2025-04-15 21:26:36 -0300

    exercicios de telas de cadastro usando padrão dotnet mvc

---
+++ atividade_15_04_2025/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace atividade_15_04_2025.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
